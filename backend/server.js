a2deef276b514a500c97638e66621d741c52f553
Josiah Nyakwara <josiejosiah89@gmail.com>
2025-11-16 03:19:03 +0300

    Add additional CORS origin for localhost

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,7 @@ app.use(helmet({
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'http://127.0.0.1:5173',
     'http://localhost:5174',
     'http://127.0.0.1:5174',
     'http://localhost:3000',
